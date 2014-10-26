db99f0862fb8b826b53c9507be9a35b278dbd565
Andrew Nero <andrew.nero@gmail.com>
2014-10-25 23:29:24 -0400

    updates

--- assets/js/slideshow-bg-image.js
+++ assets/js/slideshow-bg-image.js
@@ -3,12 +3,28 @@ $(function(){
     // jQuery backstretch
     if (jQuery.browser.mobile === false) 
     {
-        $.backstretch([
+        $('#rj-intro').backstretch([
 		"assets/img/intro/1.jpg"
         ,  "assets/img/intro/2.jpg"
         , 	"assets/img/intro/3.jpg"
         ,   "assets/img/intro/4.jpg"
 		,	"assets/img/intro/5.jpg"
-        ], {duration: 1500, fade: 3000});
-    }
+        ], {duration: 2500, fade: 3000});
+
+
+        $('#rj-charts').backstretch([
+		"assets/img/doug.png"
+        ,  "assets/img/katly.png"
+        ], {duration: 3500, fade: 3500});
+
+		
+		
+		$('#rj-charts2').backstretch([
+		"assets/img/main1.png"
+        ,  "assets/img/main1.png"
+        ], {duration: 3500, fade: 3500});
+
+		}
+		
+		
 });
