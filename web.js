c139c638e4210ab52d503ae0bf006822db05ad74
Andrew Nero <andrew.nero@gmail.com>
2015-01-30 23:16:36 -0500

    email

--- web.js
+++ web.js
@@ -11,9 +11,9 @@ var postmark = require("postmark")("ff4a1e02-a2bf-4b6a-9a1d-763471e06995")
 app.post('/getFormData', function(req, res) {
   postmark.send({
     "From": "email@plasmascape.com",
-    "To": "fighttactix@gmail.com",
+    "To": "andrew.nero@gmail.com",
     "Subject": "EMAIL FROM FightTactiX WEBSITE",
-    "TextBody": new String('From: ' + req.body.name + '.\n\rEmail: ' + req.body.email + '.\n\rPhone: ' + req.body.phone + '.\n\rShirt Size: ' + req.body.shirt)
+    "TextBody": new String('From: ' + req.body.name + '.\n\rEmail: ' + req.body.email + '.\n\rSubject: ' + req.body.subject + '.\n\rMessage: ' + req.body.message)
 	}, function(error, success) {
     if(error) {
         res.send("We have had technical difficulties and we regret to say you email hasn't been delivered to Cannon Ridge");
